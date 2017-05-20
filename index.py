901edeae4170a716f3ab036d736aee49af57262c
ankanch <kanchisme@gmail.com>
2017-05-20 12:08:24 +0800

    complete result page and view switch

--- index.py
+++ index.py
@@ -10,7 +10,14 @@ def index():
 
 @app.route('/result')
 def result():
-    return render_template("result.html")
+    test_set=[[0,[["P15891","Q06604",427,436,"PAIPQKKSFL",0.94,4,0.95,4,1.0],
+                    ["P15891","Q06604",427,436,"PAIPQKKSFL",0.94,4,0.95,4,1.0],
+                    ["P15891","Q06604",427,436,"PAIPQKKSFL",0.94,4,0.95,4,1.0],
+                    ["P15891","Q06604",427,436,"PAIPQKKSFL",0.94,4,0.95,4,1.0]]],
+               [1,[["P15800","Q06694",427,436,"PAIPQKKSFL",0.94,4,0.95,4,1.0],
+                    ["P15800","Q06694",427,436,"PASDKYLFMS",0.94,4,0.95,4,1.0],
+                    ["P15800","Q06694",427,436,"PMSHTKSALN",0.94,4,0.95,4,1.0]]]]
+    return render_template("result.html",result_package=test_set)
 
 
 if __name__ == '__main__':
