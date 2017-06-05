a7101e7c3791000b4bb692b3f73b1de2afc28200
ankanch <kanchisme@gmail.com>
2017-06-05 10:56:35 +0800

    UPDATE SessionManager

--- Utilities/CallAnalyze.py
+++ Utilities/CallAnalyze.py
@@ -10,3 +10,16 @@ PATH_DOMAIN = "../data/domain.txt"
 # constant for mode selection
 MODE_USE_BUILT_IN = 1   # use built in pwms and domains
 MODE_USE_UPLOAD = 2     # use user upload files
+
+# this function is used to call analyze functions directly from DoMo-Pred source code.
+# need 4 paramaters: 
+#                   sessionid: current session id 
+#                   pwmfiles: [usebuilt-in,[file list]]             
+#                                                     - a list with two elements one boolean,one list
+#                   domainfile: [usebuilt-in,domain file name]    
+#                                                     - a list with two elements one boolean,one string
+#                   features: [feature 1, feature 2, ... ,feature n]
+#                                                     - a list of features with the type of string.
+#                                                       decided which feature used for analyze
+def Analyzer(sessionid,pwmfiles,domainfile,features):
+    pass
--- Utilities/SessionManager.py
+++ Utilities/SessionManager.py
@@ -1,6 +1,18 @@
 import random
 import string
 import time
+import datetime
+
+# NOTE: we are manage session in a text file which one line 
+# in that file stands for one session. 
+# Session format as below:
+#                           session id , created date
+# when a new session generated ,we will add it to the file
+# if a session out of date ,we will have to delete them as well
+# MAXIUM SESSION LIFTTIME: 7 days
+
+# path of session list file
+PATH_SESSION_FILE = "../data/SESSION.session"
 
 #gernerate session id for better organised data
 def generateSessionID():
@@ -8,3 +20,56 @@ def generateSessionID():
     x += ''.join(random.sample(string.ascii_letters+string.digits, 16))
     x += str(time.time()).replace('.','') #add time in case of the same id
     return  x
+
+# this function is used to add a session to the 
+# session list file for easy manage
+def addSession(sessionid):
+    f = open("SESSION.session","a+")
+    f.write(sessionid + "," + datetime.datetime.strftime("YY-mm-dd HH:MM:ss") + "\r\n")
+    f.close()
+
+# this function is used to delete sessions from the 
+# session list file which usally happens when an session out of date 
+def deleteSession(sessionids):
+    f = open("SESSION.session","w")
+    sessiondata = f.read()
+    # start delete
+    for session in sessionids:
+        if sessiondata.find(session) == True:
+            sessiondata = sessiondata[:sessiondata.find(session)-1] + \
+                            sessiondata[sessiondata.rfind("\r\n",sessiondata.rfind(session)):]
+    print(sessiondata)
+    f.seek(0)
+    f.write(sessiondata)
+    f.close()
+
+
+# this function is used to get all sessions that out of date,
+#  we can then delete them easilt later by calling deleteSession() looply
+def getOutofDateSessionList():
+    ntime = datetime.datetime.now()
+    f = open("SESSION.session","r")
+    sessiondata = f.read()
+    f.close()
+    delta = 7 * 3600 * 24
+    sessionlist = sessiondata.split("\r\n")
+    OOD_list = []
+    for session in sessionlist:
+        if ntime - datetime.datetime.strptime("YY-mm-dd HH:MM:ss") > delta:
+            OOD_list.append(session[0])
+    return OOD_list    
+    
+
+
+# code below is used for test if this module works as expect
+if __name__ == "__main__":
+    test_session_data = """x1,2016-5-6 15:42:30\r\nx2,2017-4-21 6:23:00\r\nx4,2017-6-1 5:42:06"""
+    print(test_session_data)
+    for session in ["x2"]:
+        print(session)
+        if test_session_data.find(session) >= 0 :
+            print("1")
+            test_session_data = test_session_data[:test_session_data.find(session)-1] + \
+                                test_session_data[test_session_data.rfind("\n",test_session_data.rfind(session)):]
+    print("================")
+    print(test_session_data)
